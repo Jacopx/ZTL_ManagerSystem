6c1fbb7e834b1e025db089d99458742e064d98d2
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-01-21 09:50:19 +0100

    Fixing stream

--- src/it/polito/dp2/RNS/sol3/service/db/rnsDB.java
+++ src/it/polito/dp2/RNS/sol3/service/db/rnsDB.java
@@ -269,16 +269,16 @@ public class rnsDB {
     //@TODO: Resolve with stream
     private Vehicles searchVehicles(ConcurrentHashMap<Long, VehicleExt> vehicles, String keyword, String state, String entrytime, String position) {
         Vehicles list = new Vehicles();
-        for(VehicleExt v:vehicles.values()) {
-            list.getVehicle().add(v.getVehicle());
-        }
-//        List newList = vehicles.values().stream()
-//                .filter(v -> (keyword == null) || v.getVehicle().getId().contains(keyword))
+//        for(VehicleExt v:vehicles.values()) {
+//            list.getVehicle().add(v.getVehicle());
+//        }
+        List newList = vehicles.values().stream()
+                .filter(v -> (keyword == null) || v.getVehicle().getId().contains(keyword))
 //                .filter(v -> (state == null) || v.getVehicle().getState().equals(state))
 //                .filter(v -> (entrytime ==  null) || v.getVehicle().getEntryTime().equals(entrytime))
 //                .filter(v -> (position == null) || v.getVehicle().getPosition().equals(position))
-//                .collect(Collectors.toList());
-//        list.getVehicle().addAll(newList);
+                .collect(Collectors.toList());
+        list.getVehicle().addAll(newList);
         return list;
     }
 
