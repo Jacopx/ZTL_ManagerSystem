30112033530bfda4952fd487b72a397b9a960b8c
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-01-21 12:14:50 +0100

    Reordering vehicle paths computations

--- src/it/polito/dp2/RNS/sol3/service/db/VehicleExt.java
+++ src/it/polito/dp2/RNS/sol3/service/db/VehicleExt.java
@@ -14,6 +14,7 @@ public class VehicleExt {
     private long id;
     private Vehicle vehicle;
     private Set<List<String>> paths;
+    private ShortPaths shortPaths;
 
     public VehicleExt(long id, Vehicle vehicle) {
         this.id = id;
@@ -29,15 +30,7 @@ public class VehicleExt {
     }
 
     public Vehicle getVehicle() {
-        Vehicle v = new Vehicle();
-        ShortPaths newPaths = new ShortPaths();
-        for(List<String> ps:paths) {
-            SuggPath newPath = new SuggPath();
-            newPath.getRelation().addAll(ps);
-            newPaths.getSuggPath().add(newPath);
-        }
-        v.getShortPaths().add(newPaths);
-        return v;
+        return vehicle;
     }
 
     public void setVehicle(Vehicle vehicle) {
@@ -49,6 +42,13 @@ public class VehicleExt {
     }
 
     public void setPaths(Set<List<String>> paths) {
-        this.paths = paths;
+        ShortPaths newPaths = new ShortPaths();
+        for(List<String> ps:paths) {
+            SuggPath newPath = new SuggPath();
+            newPath.getRelation().addAll(ps);
+            newPaths.getSuggPath().add(newPath);
+        }
+        vehicle.getShortPaths().add(newPaths);
+        this.shortPaths = newPaths;
     }
 }
