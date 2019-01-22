398988cb3004d67e26f448f8627f253a1e1f7f51
Jacopo Nasi <jacopo.nasi@gmail.com>
2019-01-22 23:24:01 +0100

    loading localDB

--- src/it/polito/dp2/RNS/sol3/admClient/AdmClientPersonal.java
+++ src/it/polito/dp2/RNS/sol3/admClient/AdmClientPersonal.java
@@ -3,6 +3,7 @@ package it.polito.dp2.RNS.sol3.admClient;
 import it.polito.dp2.RNS.*;
 import it.polito.dp2.RNS.lab3.ServiceException;
 import it.polito.dp2.RNS.sol1.*;
+import it.polito.dp2.RNS.sol1.jaxb.Roads;
 import it.polito.dp2.RNS.sol3.rest.service.jaxb.*;
 
 import javax.ws.rs.client.Client;
@@ -23,6 +24,12 @@ import java.util.stream.Collectors;
 public class AdmClientPersonal implements it.polito.dp2.RNS.lab3.AdmClient {
     private String BASE;
     private RnsReader rnsReader;
+    private Set<PlaceReader> places;
+    private Set<GateReader> gates;
+    private Set<ParkingAreaReader> parkings;
+    private Set<RoadSegmentReader> segments;
+    private Set<ConnectionReader> connections;
+    private Set<VehicleReader> vehicles;
 
     public AdmClientPersonal newAdmClient() {
         if(System.getProperty("it.polito.dp2.RNS.lab3.URL") == null) {
@@ -34,7 +41,26 @@ public class AdmClientPersonal implements it.polito.dp2.RNS.lab3.AdmClient {
 
         rnsReader = new RnsReaderPersonal();
         AdmClientPersonal monitor = new AdmClientPersonal();
+
+        places = new HashSet<>();
+        gates = new HashSet<>();
+        parkings = new HashSet<>();
+        segments = new HashSet<>();
+        connections = new HashSet<>();
+        vehicles = new HashSet<>();
+
+        loadDB();
+
         return monitor;
+    }
+
+    private void loadDB() {
+        places = rnsReader.getPlaces(null);
+        gates = rnsReader.getGates(null);
+        parkings = rnsReader.getParkingAreas(null);
+        segments = rnsReader.getRoadSegments(null);
+        connections = rnsReader.getConnections();
+        vehicles = rnsReader.getVehicles(null, null, null);
     }
 
     @Override
@@ -84,32 +110,32 @@ public class AdmClientPersonal implements it.polito.dp2.RNS.lab3.AdmClient {
 
     @Override
     public Set<PlaceReader> getPlaces(String s) {
-        return rnsReader.getPlaces(s);
+        return places.stream().filter(p -> p.getId().equals(s)).collect(Collectors.toSet());
     }
 
     @Override
     public PlaceReader getPlace(String s) {
-        return rnsReader.getPlace(s);
+        return places.stream().filter(p -> p.getId().equals(s)).findFirst().get();
     }
 
     @Override
     public Set<GateReader> getGates(GateType gateType) {
-        return rnsReader.getGates(gateType);
+        return gates.stream().filter(g -> g.getType() == gateType).collect(Collectors.toSet());
     }
 
     @Override
     public Set<RoadSegmentReader> getRoadSegments(String s) {
-        return rnsReader.getRoadSegments(s);
+        return segments.stream().filter(seg -> seg.getName().equals(s)).collect(Collectors.toSet());
     }
 
     @Override
     public Set<ParkingAreaReader> getParkingAreas(Set<String> set) {
-        return rnsReader.getParkingAreas(set);
+        return parkings.stream().filter(park -> park.getServices().contains(set)).collect(Collectors.toSet());
     }
 
     @Override
     public Set<ConnectionReader> getConnections() {
-        return rnsReader.getConnections();
+        return connections;
     }
 
     @Override
